20328366c3bfdf9483c19c161a1a69e7ae2e0e12
chayoonseong <ysc77135@gmail.com>
2020-10-03 16:26:06 +0900

    영상 불러와서 재생

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "../node_modules/video-react/dist/video-react.css";
+import { Player } from "video-react";
+class App extends Component {
+  state = {
+    videoFileURL: "",
+    videoFileObject: null,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <link rel="stylesheet" href="/css/video-react.css" />
+        <form id="videoFile">
+          <input
+            type="file"
+            name="video"
+            multiple="false"
+            onChange={(e) => {
+              this.handleVideoLoad(e);
+            }}
+          />
+        </form>
+
+        <Player
+          playsInline
+          src={this.state.videoFileURL}
+          fluid={false}
+          width={480}
+          height={272}
+        />
+      </div>
+    );
+  }
+
+  handleVideoLoad(e) {
+    console.log(e.target.files);
+    let files = e.target.files;
+    if (files.length === 1) {
+      let file = files[0];
+      this.setState({
+        videoFileURL: URL.createObjectURL(file),
+        videoFileObject: file,
+      });
+    }
+  }
 }
 
 export default App;
