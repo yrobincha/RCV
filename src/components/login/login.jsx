242f99ff4482a678ba2930a72ef137fee59b5d0d
chayoonseong <ysc77135@gmail.com>
2020-11-08 11:41:32 +0900

    divide login, people. project

---
+++ src/components/login/login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./login.module.css";
+import { GoogleLogin } from "react-google-login";
+const Login = (props) => {
+  const responseGoogle = (res) => {
+    console.log(res);
+  };
+
+  const responseFail = (err) => {
+    console.log(err);
+  };
+
+  return (
+    <div>
+      <a className={styles.login}>Login</a>
+      <GoogleLogin
+        className={styles.googleLogin}
+        clientId={process.env.REACT_APP_GOOGLE_API_KEY}
+        buttonText="Sign in with google"
+        onSuccess={responseGoogle}
+        onFailure={responseFail}
+        cookiePolicy={"single_host_origin"}
+      />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/project/project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./project.module.css";
+const Project = (props) => {
+  return (
+    <div>
+      <div>Project</div>
+    </div>
+  );
+};
+
+export default Project;
