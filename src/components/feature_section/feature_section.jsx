005783dd6fac9a45e86051aa636a034df5448011
SuYoung <jsy8492@gmail.com>
2020-11-09 18:27:19 +0900

    add login/logout

--- src/components/feature_section/feature_section.jsx
+++ src/components/feature_section/feature_section.jsx
@@ -8,23 +8,23 @@ const FeatureSection = (props) => {
 				<Col xs="6" sm="4">
 					<img className={styles.featureImage} src="/images/movie-player.png" alt="movie" />
 					<h1 className={styles.feature}>동영상 편집</h1>
-					<body className={styles.featureContent}>
+					<div className={styles.featureContent}>
 					동영상 편집을 위한 기능들을 제공합니다. 실시간으로 접속중인 사람들과 작업 중인 동영상을 볼 수 있습니다.
-					</body>
+					</div>
 				</Col>
 				<Col xs="6" sm="4">
 					<img className={styles.featureImage} src="/images/chat.png" alt="chat" />
 					<h1 className={styles.feature}>실시간 피드백</h1>
-					<body className={styles.featureContent}>
+					<div className={styles.featureContent}>
 					실시간 피드백 기능을 제공하여 협업의 효율을 높여줍니다. 
-					</body>
+					</div>
 				</Col>
 				<Col sm="4">
 					<img className={styles.featureImage} src="/images/collaboration-icon.png" alt="collaboration" />
 					<h1 className={styles.feature}>작업물 공유</h1>
-					<body className={styles.featureContent}>
+					<div className={styles.featureContent}>
 					편집이 완료된 작업물을 SNS 서비스를 통해 간편하게 공유할 수 있습니다.
-					</body>
+					</div>
 				</Col>
 			</Row>
 		</Container>
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './login.module.css';
 import { GoogleLogin } from 'react-google-login';
 const Login = (props) => {
-	const responseGoogle = (res) => {
-		console.log(res);
+	const [isLoggedIn, setLoggedIn] = useState(false);
+	const [userName, setUserName] = useState('');
+
+	const setLoggedOut = () => {
+		setLoggedIn(false);
+	}
+
+	const responseGoogle = (user) => {
+		console.log('Signed in as ' + user.getBasicProfile().getName());
+		const name = user.getBasicProfile().getName();
+		// userName = user.getBasicProfile.getName();
+		setLoggedIn(true);
+		setUserName(name);
+		// userList.push(userName);
 	};
 
 	const responseFail = (err) => {
@@ -12,15 +24,26 @@ const Login = (props) => {
 
 	return (
 		<div>
-			<a className={styles.login}>로그인</a>
-			<GoogleLogin
-				className={styles.googleLogin}
-				clientId={process.env.REACT_APP_GOOGLE_API_KEY}
-				buttonText="Sign in with google"
-				onSuccess={responseGoogle}
-				onFailure={responseFail}
-				cookiePolicy={'single_host_origin'}
-			/>
+			{isLoggedIn ?
+				<>
+				<a className={styles.userName}>Hello {userName}</a>
+				<a className={styles.login}
+					onClick={setLoggedOut}>
+						로그아웃
+						</a>
+				</> :
+				<>
+				<a className={styles.login}>로그인</a>
+					<GoogleLogin
+					className={styles.googleLogin}
+					clientId={process.env.REACT_APP_GOOGLE_API_KEY}
+					buttonText="Sign in with google"
+					onSuccess={responseGoogle}
+					onFailure={responseFail}
+					cookiePolicy={'single_host_origin'}
+					/>
+				</>
+			}
 		</div>
 	);
 };
