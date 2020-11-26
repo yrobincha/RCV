e2f6d293b43df39e648ca7ba1878adc6aded376c
chayoonseong <ysc77135@gmail.com>
2020-11-26 19:53:37 +0900

    add project with name

--- react/newProject/NewProjectDialog.js
+++ react/newProject/NewProjectDialog.js
@@ -1,103 +1,114 @@
-import React, { Component } from 'react';
-import Modal from 'react-modal';
-import { server } from '../../config';
-import FetchErrorDialog from '../editor/FetchErrorDialog';
-import Header from '../editor/Header';
-import SignIn from '../editor/SignIn';
+import React, { Component } from "react";
+import Modal from "react-modal";
+import { server } from "../../config";
+import FetchErrorDialog from "../editor/FetchErrorDialog";
+import Header from "../editor/Header";
+import SignIn from "../editor/SignIn";
 
 Modal.setAppElement(document.body);
 
 export default class NewProjectDialog extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			showFetchError: false,
-			fetchError: '',
-			isModalOpen: false
-		};
+    this.state = {
+      showFetchError: false,
+      fetchError: "",
+      isModalOpen: false,
+    };
 
-		this.closeFetchErrorDialog = this.closeFetchErrorDialog.bind(this);
-	}
+    this.closeFetchErrorDialog = this.closeFetchErrorDialog.bind(this);
+  }
 
-	createProject() {
-		const url = `${server.apiUrl}/project`;
-		const params = {
-			method: 'POST',
-			headers: {
-			 	'Accept':  'application/json',
-			 	'Content-Type': 'application/json',
-			},
-			credentials: 'include'
-		};
+  createProject(projectName) {
+    const data = { projectName: projectName };
+    const url = `${server.apiUrl}/project`;
+    const params = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    };
 
-		fetch(url, params)
-			.then((response) => response.json())
-			.then((data) => {
-				if (typeof data.err === 'undefined') {
-					window.location = `${server.serverUrl}/project/${data.project}`;
+    fetch(url, params)
+      .then((response) => response.json())
+      .then((data) => {
+        if (typeof data.err === "undefined") {
+          window.location = `${server.serverUrl}/project/${data.project}`;
+        } else {
+          alert(`${data.err}\n\n${data.msg}`);
+        }
+      })
+      .catch((error) => this.openFetchErrorDialog(error.message));
+  }
 
-				} else {
-					alert(`${data.err}\n\n${data.msg}`);
-				}
-			})
-			.catch((error) => this.openFetchErrorDialog(error.message));
-	}
+  /**
+   * Show Connection error dialog
+   *
+   * @param {String} msg
+   */
+  openFetchErrorDialog(msg) {
+    this.setState({
+      showFetchError: true,
+      fetchError: msg,
+    });
+  }
 
-	/**
-	 * Show Connection error dialog
-	 *
-	 * @param {String} msg
-	 */
-	openFetchErrorDialog(msg) {
-		this.setState({
-			showFetchError: true,
-			fetchError: msg
-		});
-	}
+  /**
+   * Close Connection error dialog
+   */
+  closeFetchErrorDialog() {
+    this.setState({
+      showFetchError: false,
+      fetchError: "",
+    });
+  }
 
-	/**
-	 * Close Connection error dialog
-	 */
-	closeFetchErrorDialog() {
-		this.setState({
-			showFetchError: false,
-			fetchError: ''
-		});
-	}
+  openModal() {
+    this.setState({ isModalOpen: true });
+  }
 
-	openModal() {
-		this.setState({ isModalOpen: true });
-	}
+  closeModal() {
+    this.setState({ isModalOpen: false });
+  }
 
-	closeModal() {
-		this.setState({ isModalOpen: false });
-	}
-
-	render() {
-		return (
-			<div>
-				{this.state.showFetchError && (
-					<FetchErrorDialog msg={this.state.fetchError} onClose={this.closeFetchErrorDialog} />
-				)}
-				<Header />
-				<Modal isOpen={true} contentLabel="new project" className={'modal'} overlayClassName={'null'}>
-					<h2 className={'logo'}>
-						<img src={'/icons/favicon.svg'} alt={'logo'} />
-						RCV
-					</h2>
-					<div>
-						<button id={'create-project-button'} onClick={() => this.openModal()}>
-							시작하기
-						</button>
-						<SignIn
-							isOpen={this.state.isModalOpen}
-							close={() => this.closeModal()}
-							create={() => this.createProject()}
-						/>
-					</div>
-				</Modal>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {this.state.showFetchError && (
+          <FetchErrorDialog
+            msg={this.state.fetchError}
+            onClose={this.closeFetchErrorDialog}
+          />
+        )}
+        <Header />
+        <Modal
+          isOpen={true}
+          contentLabel="new project"
+          className={"modal"}
+          overlayClassName={"null"}
+        >
+          <h2 className={"logo"}>
+            <img src={"/icons/favicon.svg"} alt={"logo"} />
+            RCV
+          </h2>
+          <div>
+            <button
+              id={"create-project-button"}
+              onClick={() => this.openModal()}
+            >
+              시작하기
+            </button>
+            <SignIn
+              isOpen={this.state.isModalOpen}
+              close={() => this.closeModal()}
+              create={(projectName) => this.createProject(projectName)}
+            />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
