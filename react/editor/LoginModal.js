2ff7459a94e875eeb75389cedeeb685aad2e39ae
Suyoung <jsy19970512@naver.com>
2020-12-05 17:12:55 +0900

    add main login modal

--- react/editor/LoginModal.js
+++ react/editor/LoginModal.js
@@ -1,21 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SignInWithGoogle from './SignInWithGoogle';
 import SignInWithKakao from './SignInWithKakao';
-
+import styled from 'styled-components';
 const LoginModal = ({ isModalOpen, closeModal, onLogin }) => {
+	useEffect(() => {
+		// document.body.style.cssText = `position: fixed; top: -${window.scrollY}px`;
+		// return () => {
+		// 	const scrollY = document.body.style.top;
+		// 	document.body.style.cssText = `position: ""; top: "";`;
+		// 	window.scrollTo(0, parseInt(scrollY || '0') * -1);
+		// };
+	}, []);
 	return (
 		<>
 			{isModalOpen ? (
-				<div className={'login-failed-modal'}>
-					<h1 onClick={closeModal} className={'title'}>
-						시작하기
-					</h1>
-					<SignInWithGoogle onLogin={onLogin} closeModal={closeModal} isModalOpen={isModalOpen} />
-					<SignInWithKakao onLogin={onLogin} closeModal={closeModal} isModalOpen={isModalOpen} />
+				// <div className={'login-failed-modal'}>
+				<div className={'login-modal'}>
+					<ModalOverlay />
+					<ModalWrapper>
+						<ModalInner>
+							<div className={'close-button-container'}>
+								<button className={'close-button'} onClick={closeModal}>
+									X
+								</button>
+							</div>
+							<h1 onClick={closeModal} className={'title'}>
+								시작하기
+							</h1>
+							<SignInWithGoogle onLogin={onLogin} closeModal={closeModal} isModalOpen={isModalOpen} />
+							<SignInWithKakao onLogin={onLogin} closeModal={closeModal} isModalOpen={isModalOpen} />
+						</ModalInner>
+					</ModalWrapper>
 				</div>
 			) : null}
 		</>
 	);
 };
+
+const ModalOverlay = styled.div`
+	box-sizing: border-box;
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.6);
+	z-index: 999;
+`;
+
+const ModalWrapper = styled.div`
+	box-sizing: border-box;
+	position: fixed;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	z-index: 1000;
+	overflow: auto;
+	outline: 0;
+`;
+
+const ModalInner = styled.div`
+	box-sizing: border-box;
+	position: relative;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+	background-color: #161616;
+	border-radius: 10px;
+	width: 320px;
+	max-width: 480px;
+	height: 300px;
+	top: 50%;
+	transform: translateY(-50%);
+	margin: 0 auto;
+	padding: 40px 20px;
+`;
 
 export default LoginModal;
