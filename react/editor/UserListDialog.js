b300ebc954a82ca24d10b8b263d1937f4dd2cdfb
chayoonseong <ysc77135@gmail.com>
2020-12-16 02:57:33 +0900

    user list 겹침 해결

--- react/editor/UserListDialog.js
+++ react/editor/UserListDialog.js
@@ -1,61 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class UserListDialog extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			isPopoverOpen: false,
-			id: '',
-			provider: '',
-			userList: []
-		};
+    this.state = {
+      isPopoverOpen: false,
+      id: "",
+      provider: "",
+      userList: [],
+    };
 
-		this.onHover = this.onHover.bind(this);
-		this.onHoverLeave = this.onHoverLeave.bind(this);
-	}
+    this.onHover = this.onHover.bind(this);
+    this.onHoverLeave = this.onHoverLeave.bind(this);
+  }
 
-	componentDidMount() {
-		const userId = window.localStorage.getItem('id');
-		const prov = window.localStorage.getItem('provider');
-		this.setState({
-			id: userId,
-			provider: prov
-		});
-	}
+  componentDidMount() {
+    const userId = window.localStorage.getItem("id");
+    const prov = window.localStorage.getItem("provider");
+    this.setState({
+      id: userId,
+      provider: prov,
+    });
+  }
 
-	onHover() {
-		this.setState({
-			isPopoverOpen: true
-		});
-	}
+  onHover() {
+    this.setState({
+      isPopoverOpen: true,
+    });
+  }
 
-	onHoverLeave() {
-		this.setState({
-			isPopoverOpen: false
-		});
-	}
-	render() {
-		let userList = this.props.userList.map((user) => (
-			<div key={user} className={'user-list'}>
-				<div className={'user-container'}>
-					<img className={'profile-image'} src="../images/sample-profile.jpeg" alt="Img" />
-					<p className={'user'}>{user}</p>
-				</div>
-			</div>
-		));
+  onHoverLeave() {
+    this.setState({
+      isPopoverOpen: false,
+    });
+  }
+  render() {
+    let userList = this.props.userList.map((user) => (
+      <div key={user} className={"user-container"}>
+        <img
+          className={"profile-image"}
+          src="../images/sample-profile.jpeg"
+          alt="Img"
+        />
+        <p className={"user"}>{user}</p>
+      </div>
+    ));
 
-		return (
-			<>
-				<div className={'user-list-container'}>
-					<button className={'user-list-button'} onMouseEnter={this.onHover} onMouseLeave={this.onHoverLeave}>
-						참여자 목록
-					</button>
-				</div>
-				{this.state.isPopoverOpen && <>{userList}</>}
-			</>
-		);
-	}
+    return (
+      <>
+        <div className={"user-list-container"}>
+          <button
+            className={"user-list-button"}
+            onMouseEnter={this.onHover}
+            onMouseLeave={this.onHoverLeave}
+          >
+            참여자 목록
+          </button>
+        </div>
+        {this.state.isPopoverOpen && (
+          <div className={"user-list"}>{userList}</div>
+        )}
+      </>
+    );
+  }
 }
 
 export default UserListDialog;
